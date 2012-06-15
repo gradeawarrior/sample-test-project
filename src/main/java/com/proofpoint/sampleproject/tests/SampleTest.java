75d369a33eaca058732ebc90eede460557cadf08
Peter Salas <psalas@proofpoint.com>
2012-06-15 14:30:46 -0700

    Adding an example HTTP test

--- src/main/java/com/proofpoint/sampleproject/tests/SampleTest.java
+++ src/main/java/com/proofpoint/sampleproject/tests/SampleTest.java
@@ -1,9 +1,18 @@
 package com.proofpoint.sampleproject.tests;
 
+import com.proofpoint.http.client.ApacheHttpClient;
+import com.proofpoint.http.client.StatusResponseHandler.StatusResponse;
 import org.testng.annotations.Test;
+
+import java.net.URI;
+
+import static com.proofpoint.http.client.Request.Builder.prepareGet;
+import static com.proofpoint.http.client.StatusResponseHandler.createStatusResponseHandler;
+import static org.testng.Assert.assertEquals;
 
 public class SampleTest
 {
+    ApacheHttpClient client = new ApacheHttpClient();
 
     @Test
     public void myTest1() {}
@@ -13,4 +22,15 @@ public class SampleTest
 
     @Test
     public void myTest3() {}
+
+    @Test
+    public void myHTTPTest()
+            throws Exception
+    {
+        StatusResponse response = client.execute(
+                prepareGet().setUri(new URI("http://www.google.com")).build(),
+                createStatusResponseHandler());
+
+        assertEquals(response.getStatusCode(), 200);
+    }
 }
